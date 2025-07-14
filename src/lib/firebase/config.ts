468e5237494789a5749127639359fa4a3136df87
Neeraj <neerajgouda8@gmail.com>
2025-07-14 15:43:44 +0000

    i see same error Key is not vaild

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCzwe1PVrKqQd7z7UWkLz0qOL6Dp_uVkfc",
+  authDomain: "campusconnect-92d97.firebaseapp.com",
+  projectId: "campusconnect-92d97",
+  storageBucket: "campusconnect-92d97.appspot.com",
+  messagingSenderId: "986369405803",
+  appId: "1:986369405803:web:ce3822bda01d6b117b122e",
 };
 
 // Check if the config is loaded
